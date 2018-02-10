998e148cac206f435c6c974690133cb5352a9dbd
Ben Harrington <harringtonben@icloud.com>
2018-02-10 10:29:45 -0600

    working through unit tests

---
+++ Stuff.App/NameParser.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stuff.App
+{
+    public class NameParser
+    {
+        public Name Parse(string name)
+        {
+
+            if (name.Contains(" "))
+            {
+                var nameParts = name.Split(' ');
+
+                return new Name
+                {
+                    FirstName = nameParts[0],
+                    LastName = nameParts[1]
+                };
+            }
+            else
+            {
+                return new Name
+                {
+                    FirstName = name
+                };
+            }
+
+
+        }
+    }
+
+    public class Name
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+    }
+}
